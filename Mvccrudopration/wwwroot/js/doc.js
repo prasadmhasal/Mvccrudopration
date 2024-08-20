84069fb9e4ada4d6fb6e4dc438cf0db8bf54daa4
LENOVO <LENOVO@LAPTOP-Q28PNCVP>
2024-08-20 14:06:51 +0530

    ajax

--- Mvccrudopration/wwwroot/js/doc.js
+++ Mvccrudopration/wwwroot/js/doc.js
@@ -1,34 +1,42 @@
 ﻿$(document).ready(function () {
-
+    alert("Hello");
 });
 
 $('#btn1').click(function () {
-    $('#exampleModal').modal('show')
+    $('#exampleModal').modal('show');
 });
 
-$('#save').click(function () {
 
-    var obj = {
-        Name: $('#Name').val(),
-        Email: $('#Email').val(),
-        Salary: $('#Salary').val()
-    }
-    $.ajax({
-        url: '/Ajax/AddEmp',
-        type: 'POST',
-        dataType: 'json',
-        contentType: 'appliaction/x-www-form-urlencoded;charset=utf8',
-        data: obj,
-        success: function () {
-            alert("Emp Added Successfully");
-            $('#exampleModal').modal('hide');
-        },
-        error: function () {
-            alert("Something wrong");
-        }
-    }
+$('#test').click(function () {
+    alert("Hello");
+});
+$('#savebtn').click(function () {
+    alert("Hello");
+    //var obj = {
+    //    Name: $('#Name').val(),
+    //    Email: $('#Email').val(),
+    //    Salary: $('#Salary').val()
+    //}
+    //var obj2 = $('#myform').serialize();
+    //console.log(obj);
+    //console.log(obj2);
 
-    )
+    //$.ajax({
+    //    url: '/Ajax/AddEmp',
+    //    type: 'POST',
+    //    dataType: 'json',
+    //    contentType: 'appliaction/x-www-form-urlencoded;charset=utf8',
+    //    data: obj2,
+    //    success: function () {
+    //        alert("Emp Added Successfully");
+    //        $('#exampleModal').modal('hide');
+    //    },
+    //    error: function () {
+    //        alert("Something wrong");
+    //    }
+    //}
+
+    //)
 });
 
 
